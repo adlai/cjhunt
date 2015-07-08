713f638a53e9e751438d7ce59601a265272d2b60
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2015-07-08 13:11:10 +0300

    fix calls to bitcoind.rpc + add comments

--- cjhunt.lisp
+++ cjhunt.lisp
@@ -1,4 +1,7 @@
-(use-package (cons :json-rpc (ql:quickload '(:cl-json :drakma :parse-float :alexandria :local-time))))
+(defpackage :cjhunt
+  (:use :cl :json-rpc .
+        #.(ql:quickload '(:cl-json :drakma :parse-float :alexandria :local-time))))
+(in-package :cjhunt)
 
 (defclass bitcoind ()
   ((url :type string :initarg :url) (stream :type stream)
@@ -19,39 +22,56 @@
         (if closep (close body) (read-line (setf stream redundant)))
         (if error (error "bitcoind error: ~S" error) result)))))
 
-(defun coinjoinp (txid &aux (tx (bitcoind.rpc "getrawtransaction" txid 1)))
-  (let ((ins (cdr (assoc :vin tx))) (outs (cdr (assoc :vout tx))))
+(defvar *node*                          ; edit these!
+  (make-instance 'bitcoind :url "http://localhost:8332" ; or wherever
+                 :auth '("rpcuser" "rpcpassword"))) ; from bitcoin.conf
+
+;;; we are primarily looking for joinmarket coinjoins
+(defun coinjoinp (txid)
+  (let* ((tx (bitcoind.rpc *node* "getrawtransaction" txid 1))
+         (ins (cdr (assoc :vin tx))) (outs (cdr (assoc :vout tx))))
     (cond
+      ;; ignore the coinbase transaction
       ((find :coinbase ins :key #'caar) (warn "~A coinbase" txid))
+      ;; ignore transactiosn which have multisig inputs, since the current
+      ;; joinmarket doesn't use multisig (but could have a multisig output)
       ((find-if (lambda (asm) (string= "0 " asm :end2 2))
                 ins :key (lambda (in)
                            (cdr (assoc :asm (cdr (assoc :script-sig in))))))
        (warn "~A has multisig inputs" txid))
       (t (let* ((outsizes (mapcar #'cdar outs))
+                ;; look for repeated outputs with identical size
                 (cjouts (loop for out in outs for amt = (cdar out)
                            when (> (count amt outsizes) 1) collect out))
+                ;; ignore duplicate input public keys
                 (pks (delete-duplicates (mapcar (compose #'cdar #'cdaddr)
                                                 ins) :test #'string=)))
            (if (null cjouts) (warn "~A no duplicate output sizes" txid)
                (let ((n-pks (length pks)) (n-cjouts (length cjouts)))
+                 ;; the number of distinct public keys is an upper bound on the
+                 ;; number of distinct parties involved in the transaction
                  (if (< n-pks n-cjouts)
                      (warn "~A inputs ~D < ~D cjouts" txid n-pks n-cjouts)
                      (let ((cjout-sizes (mapcar #'cdar cjouts)))
                        (if (apply #'= cjout-sizes)
                            (cons n-cjouts (car cjout-sizes))
+                           ;; a nontrivial coinjoin has multiple candidates for
+                           ;; the actual output size. ignore them, for now.
                            (warn "~A nontrivial cj" txid)))))))))))
 
-(defun blockjoins (blkid &aux (blk (bitcoind.rpc "getblock" blkid)))
+(defun blockjoins (blkid &aux (blk (bitcoind.rpc *node* "getblock" blkid)))
   (values (loop for txid in (cdr (assoc :tx blk))
              for cjp = (handler-bind ((warning #'muffle-warning))
                          (coinjoinp txid)) when cjp collect (cons txid cjp))
           (cdr (assoc :previousblockhash blk))
           (cdr (assoc :nextblockhash blk))))
 
+;;; hardcoded script: searches from the best block backwards, accumulating
+;;; the coinjoins in each block, grouped by blockid. collects 10 such blocks.
 (let (all-joins (blkid (cdr (assoc :bestblockhash
-                                   (bitcoind.rpc "getblockchaininfo")))))
+                                   (bitcoind.rpc *node* "getblockchaininfo")))))
   (time (loop
            (multiple-value-bind (joins previd) (time (blockjoins blkid))
              (print (length (and joins (push (cons blkid joins) all-joins))))
-             (if (or (null previd) (= (length all-joins) 5000))
-                 (return all-joins) (setf blkid previd))))))
+             (if (= (length all-joins) 10)
+                 (return (pprint all-joins)) (setf blkid previd))))))
