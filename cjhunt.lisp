8ec7ca6d5b9ba769968ba03e786ea03f050233ac
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2015-08-04 13:04:26 +0300

    split out coinjoinp-cdr for non-coinbase txs

--- cjhunt.lisp
+++ cjhunt.lisp
@@ -26,19 +26,16 @@
   (make-instance 'bitcoind :url "http://localhost:8332" ; or wherever
                  :auth '("rpcuser" "rpcpassword"))) ; from bitcoin.conf
 
-;;; we are primarily looking for joinmarket coinjoins
-(defun coinjoinp (txid)
-  (let* ((tx (bitcoind.rpc *node* "getrawtransaction" txid 1))
-         (ins (cdr (assoc :vin tx))) (outs (cdr (assoc :vout tx))))
+(defun coinjoinp-cdr (id &optional
+                           (tx (bitcoind.rpc *node* "getrawtransaction" id 1)))
+  (let ((ins (cdr (assoc :vin tx))) (outs (cdr (assoc :vout tx))))
     (cond
-      ;; ignore the coinbase transaction
-      ((find :coinbase ins :key #'caar) (warn "~A coinbase" txid))
       ;; ignore transactiosn which have multisig inputs, since the current
       ;; joinmarket doesn't use multisig (but could have a multisig output)
       ((find-if (lambda (asm) (string= "0 " asm :end2 2))
                 ins :key (lambda (in)
                            (cdr (assoc :asm (cdr (assoc :script-sig in))))))
-       (warn "~A has multisig inputs" txid))
+       (warn "~A has multisig inputs" id))
       (t (let* ((outsizes (mapcar #'cdar outs))
                 ;; look for repeated outputs with identical size
                 (cjouts (loop for out in outs for amt = (cdar out)
@@ -46,23 +43,30 @@
                 ;; ignore duplicate input public keys
                 (pks (delete-duplicates (mapcar (compose #'cdar #'cdaddr)
                                                 ins) :test #'string=)))
-           (if (null cjouts) (warn "~A no duplicate output sizes" txid)
+           (if (null cjouts) (warn "~A no duplicate output sizes" id)
                (let ((n-pks (length pks)) (n-cjouts (length cjouts)))
                  ;; the number of distinct public keys is an upper bound on the
                  ;; number of distinct parties involved in the transaction
                  (if (< n-pks n-cjouts)
-                     (warn "~A inputs ~D < ~D cjouts" txid n-pks n-cjouts)
+                     (warn "~A inputs ~D < ~D cjouts" id n-pks n-cjouts)
                      (let ((cjout-sizes (mapcar #'cdar cjouts)))
                        (if (apply #'= cjout-sizes)
                            (cons n-cjouts (car cjout-sizes))
                            ;; a nontrivial coinjoin has multiple candidates for
                            ;; the actual output size. ignore them, for now.
-                           (warn "~A nontrivial cj" txid)))))))))))
+                           (warn "~A nontrivial cj" id)))))))))))
+
+;;; we are primarily looking for joinmarket coinjoins
+(defun coinjoinp (txid &aux)
+  (let ((tx (bitcoind.rpc *node* "getrawtransaction" txid 1)))
+    ;; ignore the coinbase transaction
+    (if (find :coinbase (cdr (assoc :vin tx)) :key #'caar) (warn "~A coinbase" txid)
+        (coinjoinp-cdr txid tx))))
 
 (defun blockjoins (blkid &aux (blk (bitcoind.rpc *node* "getblock" blkid)))
-  (values (loop for txid in (cdr (assoc :tx blk))
+  (values (loop for txid in (cddr (assoc :tx blk)) ; cddr skips coinbase txs
              for cjp = (handler-bind ((warning #'muffle-warning))
-                         (coinjoinp txid)) when cjp collect (cons txid cjp))
+                         (coinjoinp-cdr txid)) when cjp collect (cons txid cjp))
           (cdr (assoc :previousblockhash blk))
           (cdr (assoc :nextblockhash blk))))
 
