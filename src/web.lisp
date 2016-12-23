6849d39fd72077a77bb1298b606ebddfb95f0d97
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2016-12-23 04:11:30 +0200

    defroute "/flush?"

--- src/web.lisp
+++ src/web.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-user)
 (defpackage cjhunt.web
-  (:use :cl
+  (:use :cl :anaphora
         :caveman2 :caveman2.exception
         :cjhunt.config
         :cjhunt.view
@@ -31,6 +31,24 @@
 (defroute ("/block(joins)?" :regexp t) (&key |id|)
   (handler-case (render-json (blockjoins |id|))
     (error () (error 'caveman-exception :code 404))))
+
+(defroute ("/flush?") (&key |symbol| |pass| |package|)
+  (awhen (and (string= |pass| "CHANGEME")
+              (get (find-symbol (string-upcase |symbol|)
+                                (find-package
+                                 (string-upcase (or |package| "hunt"))))
+                   'fare-memoization::memoization-info))
+    (let* ((old (fare-memoization::memoized-table it))
+           (save (loop for key being the hash-key in old using (hash-value data)
+                    when (car data) collect (cons key data))))
+      (with-output-to-string (*trace-output*)
+        (time (setf (slot-value it 'fare-memoization::table)
+                    (aprog1 (make-hash-table
+                             :rehash-size (hash-table-rehash-size old)
+                             :rehash-threshold (hash-table-rehash-threshold old)
+                             :test (hash-table-test old) :size (length save))
+                      (loop for (key . data) in save
+                         do (setf (gethash key it) data)))))))))
 
 ;;
 ;; Error pages
