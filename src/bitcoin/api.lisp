d6e1bd010dc60c65565ab1bd31147a693f34906e
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2018-03-23 13:15:43 +0300

    moving target stalking

--- src/bitcoin/api.lisp
+++ src/bitcoin/api.lisp
@@ -25,22 +25,26 @@
   (define-rpc getrawtransaction (id &optional (jsonp 1)) id jsonp)
   (define-rpc listaccounts (&key (minconf 0) (watchonly t)) minconf watchonly)
 
-  (macrolet ((porn ()	       ; if you have to ask, you'll never know
+  (macrolet ((porn ()         ; if you have to ask, you'll never know
 	       (labels ((split (delimiter sequence)
 			  (split-sequence delimiter sequence
 					  :remove-empty-subseqs t))
 			(inter (string) (intern (string-upcase string))))
 		 `(progn
 		    ,@(loop for line in (split #\Newline (rpc "help"))
-			 for (command . parameters) = (split #\  line) ;D
-			 unless (or (and (string= "==" command)      :P
-					 (string= "==" (first (last parameters))))
-				    (nth-value 1 (inter command))) collect
-			   `(define-rpc ,(inter command) () ;)
-			      ,@(loop for raw in parameters
-				   for trim = (string-trim ")\"" raw)
-				   if (string= "(" trim) collect '&optional ;(
-				   else unless (or (zerop (length trim))
-						   (string= "delta>" trim) ;<
-						   ) collect (inter trim))))))))
-    (porn)))		       ; well maybe you will, once you see it.
+			 for (command . params) = (split #\  line) ;D
+			 unless (or (and (string= "==" command)    :P
+					 (string= "==" (first (last params))))
+				    (nth-value 1 (inter command)))
+                         collect `(define-rpc ,(inter command) () ;)
+                                    ,@(loop for raw in params with crt
+                                         for snip = (string-trim ")\"" raw)
+                                         for trim = (string-trim "()" snip)
+                                         if (and (search "(" snip) (not crt)
+                                                 (setf crt t)) ;(
+                                         collect '&optional unless
+                                           (or (zerop (length snip))
+                                               (string= "delta>" snip)
+                                               (string= "(" snip)) ;<
+                                         collect (inter trim))))))))
+    (porn)))                            ; well maybe you will, once you see it.
